7cadb0eeb7dcbd5db937e6d40e42bf9afb920b1d
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2024-03-28 12:00:05 +0530

    Update Generic Installer.sh

--- Generic Installer.sh
+++ Generic Installer.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo apt update && sudo apt upgrade && bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) && sudo systemctl enable nodered.service && cd ~/.node-red && npm install @node-red-contrib-themes/theme-collection && npm install node-red-dashboard && node-red restart
+sudo apt update && sudo apt upgrade && bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) && clear && sudo systemctl enable nodered.service && cd ~/.node-red && npm install @node-red-contrib-themes/theme-collection && npm install node-red-dashboard && node-red restart
